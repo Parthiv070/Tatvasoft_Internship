e466b8ee0fa5bd3b1c4727cb3e11eec72d46c9db
Parthiv070 <parthivchudasama5@gmail.com>
2025-07-06 15:52:46 +0530

    add mission folder under the day 3

---
+++ Day 3/Mission/Mission.Entities/Migrations/20250704093359_SyncModelChanges.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mission.Entities.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncModelChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "id",
+                keyValue: 1,
+                columns: new[] { "email_address", "password" },
+                values: new object[] { "Parthiv@gmail.com", "Parthiv@123" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "id",
+                keyValue: 1,
+                columns: new[] { "email_address", "password" },
+                values: new object[] { "admin@tatvasoft.com", "Tatva@123" });
+        }
+    }
+}
