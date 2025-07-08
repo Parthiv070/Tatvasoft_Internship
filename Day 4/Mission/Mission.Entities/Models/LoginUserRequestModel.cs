325e4ec14a75a5af71b07cab55d4f07d1a2bf23d
Parthiv070 <parthivchudasama5@gmail.com>
2025-07-08 17:35:38 +0530

    Added Day 4 Mission folder

---
+++ Day 4/Mission/Mission.Entities/Models/LoginUserRequestModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Mission.Entities.Models
+{
+    public class LoginUserRequestModel
+    {
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+
+        // Add for registration
+        public string FirstName { get; set; }  // ✅ NEW FIELD
+    }
+}
